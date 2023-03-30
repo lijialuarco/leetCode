db12bfdd4452c597476130cdde0a3e14496471f6
arcoli <arcoli@tencent.com>
2023-03-30 11:48:56 +0800

    init goland master

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+func main() {
+	println(countVowelStrings(2))
+}
+
+func furthestBuilding(heights []int, bricks int, ladders int) int {
+	max := 0
+	for i := 1; i <= len(heights); i++ {
+		if heights[i] < heights[i-1] {
+			continue
+		}
+
+	}
+	for k, height := range heights {
+
+	}
+}
