f4a9d0da97891ae442f513043664f3823293f538
arcoli <arcoli@tencent.com>
2023-03-30 11:51:39 +0800

    complet

--- main.go
+++ main.go
@@ -4,15 +4,19 @@ func main() {
 	println(countVowelStrings(2))
 }
 
-func furthestBuilding(heights []int, bricks int, ladders int) int {
-	max := 0
-	for i := 1; i <= len(heights); i++ {
-		if heights[i] < heights[i-1] {
-			continue
+func countVowelStrings(n int) int {
+	vowel := []int{
+		1, 1, 1, 1, 1,
+	}
+	for i := 1; i < n; i++ {
+		for j := 1; j < 5; j++ {
+			vowel[j] += vowel[j-1]
 		}
-
 	}
-	for k, height := range heights {
-
+	ret := 0
+	for _, v := range vowel {
+		ret += v
 	}
+	return ret
+
 }
