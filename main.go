b4fe69389ddce7d0a0d2b82eb71c36771ded2d4b
arcoli <arcoli@tencent.com>
2023-03-31 16:30:00 +0800

    base solution

--- main.go
+++ main.go
@@ -1,25 +1,28 @@
 package main
 
-//
-// 给定一个正整数数组,元素重复个数只可能是两个结果,且两个结果互质,其中一个结果只出现一次
-// 要求空间复杂度为O(1) 找到这个元素
+// https://leetcode.cn/problems/widest-vertical-area-between-two-points-containing-no-points/
+// 给你 n 个二维平面上的点 points ，其中 points[i] = [xi, yi] ，请你返回两点之间内部不包含任何点的 最宽垂直区域 的宽度。
+// 垂直区域 的定义是固定宽度，而 y 轴上无限延伸的一块区域（也就是高度为无穷大）。 最宽垂直区域 为宽度最大的一个垂直区域。
+// 请注意，垂直区域 边上 的点 不在 区域内。
 func main() {
-	input := []int{1, 2, 3, 4, 5, 6, 7, 7, 6, 5, 4, 3, 2, 1, 5, 5}
-	println(findSingleCount(input, 4))
+	input := [][]int{{1, 2}, {2, 3}, {3, 4}, {4, 5}, {5, 6}, {6, 7}}
+	println(maxWidthOfVerticalArea(input))
 }
 
-func findSingleCount(nums []int, a int) int {
-	mask := 1 // 用来找到只出现一次的元素在二进制表示中的位置
-	for (a & mask) == 0 {
-		mask <<= 1
-	}
-
-	result := 0
-	for i := 0; i < len(nums); i++ {
-		if (nums[i] & mask) != 0 {
-			result ^= nums[i] // 异或操作，相同的数异或结果为0
+func maxWidthOfVerticalArea(points [][]int) int {
+	max := 0
+	for i := 0; i < len(points); i++ {
+		for j := i + 1; j < len(points); j++ {
+			if points[i][0] > points[j][0] {
+				points[i], points[j] = points[j], points[i]
+			}
 		}
 	}
 
-	return result
+	for i := 0; i < len(points)-1; i++ {
+		if points[i+1][0]-points[i][0] > max {
+			max = points[i+1][0] - points[i][0]
+		}
+	}
+	return max
 }
